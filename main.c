8b7fd2b009bc8b3b5309f7bae2c76865b9dc7e40
David Koch <davidaltenberg@googlemail.com>
2011-05-18 01:47:59 +1000

    second commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/* ================================================================== */
+/* Hello world - program                                              */
+/* filename: main.c                                                   */
+/* author: David Koch                                                 */
+/* date: 18.05.2011                                                   */
+/* Version: 0.1                                                       */
+/* ================================================================== */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "Hello World!\n";
+    return 0;
+}
